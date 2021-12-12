7183f2d3df0f977ca73e57ca59bc7e057c1642e1
acasasaez <acasasae@myuax.com>
2021-12-12 22:04:51 +0100

    comit 1, prueba para guardar ficheros en otros

---
+++ Ajedrez.py
@@ -0,0 +1,5 @@
+tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
+fichero = open ("./aje.txt","w", encoding="utf-8")
+fichero.write(tablero_inicial)
+fichero.close()
+
